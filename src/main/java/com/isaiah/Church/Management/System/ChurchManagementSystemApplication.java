7c554842a7c9709ee78933d04600e9114e523031
Isaiah Chileshe <chilesheisaiah10@gmail.com>
2026-03-19 01:36:14 +0200

    Initial commit

---
+++ src/main/java/com/isaiah/Church/Management/System/ChurchManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.isaiah.Church.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaiah/Church/Management/System/ChurchManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaiah.Church.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
